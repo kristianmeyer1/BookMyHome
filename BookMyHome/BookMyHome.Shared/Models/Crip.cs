a60083e73228b5ea3e5100d7b3f4f66f3078b08c
Anders Bøgehøj Iversen <andersbiversen35@gmail.com>
2025-10-29 13:13:42 +0100

    Added models

---
+++ BookMyHome/BookMyHome.Shared/Models/Crip.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookMyHome.Shared.Models
+{
+    public class Crip
+    {
+        [Key]
+        public int HomeId { get; set; }
+        [Required]
+        public string HomeType { get; set; }
+        [Required]
+        public string HomeAdress { get; set; }
+        [Required]
+        public int HomeSize { get; set; }
+        [Required]
+        public int HomeBeds { get; set; }
+        [Required]
+        public int HomeBathrooms { get; set; }
+        [Required]
+        public string HomeRules { get; set; }
+        [Required]
+        public int HomeStars { get; set; }
+
+        public Crip(int homeId, string homeType, string homeAdress, int homeSize, int homeBeds, int homeBathrooms, string homeRules, int homeStars)
+        {
+            HomeId = homeId;
+            HomeType = homeType;
+            HomeAdress = homeAdress;
+            HomeSize = homeSize;
+            HomeBeds = homeBeds;
+            HomeBathrooms = homeBathrooms;
+            HomeRules = homeRules;
+            HomeStars = homeStars;
+        }
+    }
+}
